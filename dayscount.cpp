5d4eb78a4c1386aefddb07c65cd42af2789e321d
bbiwannabecoder <152612159+bbiwannabecoder@users.noreply.github.com>
2023-12-10 15:09:20 +0800

    Myfirstcpp

---
+++ dayscount.cpp
@@ -0,0 +1,11 @@
+// my first program for learning Cpp
+#include <iostream>;
+	int main(){
+    using namespace std;
+    int days;
+    cout<< "How many days are there before your collage entrance exam?\n";
+    cin>> days;
+    cout<< "So you still have "<<days-1<<" to play";
+    return 0;
+
+}
